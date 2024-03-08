e3a9842a4403d2e13ea2e80ac7feb9d5822553ec
Daisuke Sakurai <d.sakurai0@gmail.com>
2024-03-08 11:22:17 +0900

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dolisp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/dolisp/core.clj
@@ -0,0 +1,24 @@
+(ns dolisp.core)
+
+(defmacro task-fn [args & body]
+  `(fn ~args
+
+    ;; Analyze args here? Or maybe do it outside fn (but still inside a quoted block) and return information as a map
+
+     (println "Arguments:" (list ~@args))
+     ;; Function body
+     ~@body
+  )
+)
+
+(defn -main []
+  "I'm the entry point for this application."
+  (;; function
+    (task-fn
+      [a b]
+      (println a b)
+    )
+    ;; arguments
+    1 2
+    )
+  )
